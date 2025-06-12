b3300c619e3bae52ac3b672fa8416dd0f978a0b9
ali <mahad.dev3@gmail.com>
2025-06-12 18:21:07 +0500

    updated messages

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,29 +1,29 @@
 import { IsString, IsNotEmpty, IsOptional, IsBoolean, IsArray, MaxLength, MinLength } from 'class-validator';
 
 export class CreatePostDto {
-  @IsString()
-  @IsNotEmpty()
-  @MinLength(3)
-  @MaxLength(200)
+  @IsString({message: 'Title must be a string'})
+  @IsNotEmpty({ message: 'Title is required' })
+  @MinLength(3, { message: 'Title must be at least 3 characters long' })
+  @MaxLength(200, { message: 'Title must be less than 200 characters' })
   title: string;
 
-  @IsString()
-  @IsNotEmpty()
-  @MinLength(10)
+  @IsString({ message: 'Content must be a string' })
+  @IsNotEmpty({ message: 'Content is required' })
+  @MinLength(10, { message: 'Content must be at least 10 characters long' })
   content: string;
 
-  @IsString()
-  @IsNotEmpty()
-  @MinLength(2)
-  @MaxLength(100)
+  @IsString({ message: 'Author must be a string' })
+  @IsNotEmpty({ message: 'Author is required' })
+  @MinLength(2, { message: 'Author must be at least 2 characters long' })
+  @MaxLength(100, { message: 'Author must be less than 100 characters' })
   author: string;
 
   @IsOptional()
-  @IsBoolean()
+  @IsBoolean({ message: 'Published must be a boolean' })
   published?: boolean;
 
   @IsOptional()
-  @IsArray()
-  @IsString({ each: true })
+  @IsArray({ message: 'Tags must be an array' })
+  @IsString({ each: true, message: 'Tags must be an array of strings' })
   tags?: string[];
 }
